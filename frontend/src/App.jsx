230022099ee1af709a080d69e11b5aad1ecbdc1a
Nadu_Dev <inbox.nadun@gmail.com>
2024-04-17 10:43:30 +0530

    frontend : added login sample and enforced folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./components/auth/login";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* login */}
+        <Route path="/login" exact element={<LoginScreen />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
